4962f00ff4cfd7352e62b8d1fbdf44c9a81c66db
HamadIslamian <132324180+Hamad345@users.noreply.github.com>
2024-07-27 01:17:21 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,47 @@
+// console.time("test")
+// var x=10;
+
+// var y="10";
+// if(x===y){
+//     document.write("hello")
+// }
+// console.log("hello")
+// console.timeEnd("test")
+// var a=10;
+// a++;
+// a--
+// document.write(a)
+// console.table(([1,2,3]))
+
+// var a=20;
+// if(a!=20){
+//     document.write("hello")
+// }
+
+// var a=10;
+// (a==20)? document.write(a):document.write("nothing")
+
+// var a=28;
+
+// switch(a){
+//     case 0: case 1: case 2:document.write("monday")
+// }
+// if (a=2){
+//     alert("THANKS")
+// }else{
+//     alert("sorry")
+// }
+// function hello(){
+//     alert("hello")
+// }
+
+// hello();
+
+// var a=[2,4,5];
+// a.forEach(multiply);
+// function multiply(item,index,arr){
+//     arr[index] = item *10;
+
+// }
+// document.write(a)
+// document.write("<li>hello</li>")
